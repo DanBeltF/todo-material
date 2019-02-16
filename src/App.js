36d761da85b554ed2160fb67f1211a6ea9c6d1ed
d.beltran <bleachstick@hotmail.com>
2019-02-16 01:03:58 -0500

    Part 1: Login

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {TodoList} from "./TodoList";
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import moment from "moment";
+import {Login} from "./component/Login";
 
 class App extends Component {
 
@@ -22,54 +23,56 @@ class App extends Component {
 
         return (
             <div className="App">
-                <header className="App-header">
+                <Login>
+                    <header className="App-header">
                     <img src={logo} className="App-logo" alt="logo"/>
                     <h1 className="App-title">TODO React App</h1>
                 </header>
 
-                <br/>
-                <br/>
-                <form onSubmit={this.handleSubmit} className="todo-form">
-                    <h3>New TODO</h3>
-                    <label htmlFor="text" className="right-margin">
-                        Text:
-                    </label>
+                    <br/>
+                    <br/>
+                    <form onSubmit={this.handleSubmit} className="todo-form">
+                        <h3>New TODO</h3>
+                        <label htmlFor="text" className="right-margin">
+                            Text:
+                        </label>
 
-                    <input
-                        id="text"
-                        onChange={this.handleTextChange}
-                        value={this.state.text}>
-                    </input>
+                        <input
+                            id="text"
+                            onChange={this.handleTextChange}
+                            value={this.state.text}>
+                        </input>
 
-                    <br/>
-                    <br/>
-                    <label htmlFor="priority" className="right-margin">
-                        Priority:
-                    </label>
+                        <br/>
+                        <br/>
+                        <label htmlFor="priority" className="right-margin">
+                            Priority:
+                        </label>
 
-                    <input
-                        id="priority"
-                        type="number"
-                        onChange={this.handlePriorityChange}
-                        value={this.state.priority}>
-                    </input>
-                    <br/>
-                    <br/>
+                        <input
+                            id="priority"
+                            type="number"
+                            onChange={this.handlePriorityChange}
+                            value={this.state.priority}>
+                        </input>
+                        <br/>
+                        <br/>
 
-                    <DatePicker
-                        id="due-date"
-                        selected={this.state.dueDate}
-                        placeholderText="Due date"
-                        onChange={this.handleDateChange}>
-                    </DatePicker>
+                        <DatePicker
+                            id="due-date"
+                            selected={this.state.dueDate}
+                            placeholderText="Due date"
+                            onChange={this.handleDateChange}>
+                        </DatePicker>
+                        <br/>
+                        <button>
+                            Add #{this.state.items.length + 1}
+                        </button>
+                    </form>
                     <br/>
-                    <button>
-                        Add #{this.state.items.length + 1}
-                    </button>
-                </form>
-                <br/>
-                <br/>
-                <TodoList todoList={this.state.items}/>
+                    <br/>
+                    <TodoList todoList={this.state.items}/>
+                </Login>
             </div>
         );
     }
